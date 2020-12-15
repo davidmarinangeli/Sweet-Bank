88829c469a8cd7f5310d3214147c2b221da20627
davidmarinangeli <david.marinangeli@gmail.com>
2020-12-15 11:38:48 +0100

    account api

--- account/src/main/java/com/davidm/account/network/AccountApi.kt
+++ account/src/main/java/com/davidm/account/network/AccountApi.kt
@@ -1,12 +1,12 @@
 package com.davidm.account.network
 
 import com.davidm.account.entities.AccountBalance
+import com.davidm.account.entities.AccountHolder
 import com.davidm.account.entities.Accounts
 import com.davidm.network.API_KEY
 import com.davidm.network.API_VERSION
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
+import okhttp3.RequestBody
+import retrofit2.http.*
 
 interface AccountApi {
 
@@ -22,4 +22,26 @@ interface AccountApi {
         @Header("Authorization") token: String = "Bearer $API_KEY",
         @Header("Accept") value: String = "application/json"
     ): AccountBalance
+
+
+    @GET("api/$API_VERSION/account-holder")
+    suspend fun getAccountHolder(
+        @Header("Authorization") token: String = "Bearer $API_KEY",
+        @Header("Accept") value: String = "application/json"
+    ): AccountHolder
+
+    @GET("api/$API_VERSION/account-holder/{accountHolder}/profile-image")
+    suspend fun getAccountPicture(
+        @Header("Authorization") token: String = "Bearer $API_KEY",
+        @Header("Accept") value: String = "application/json",
+        @Path("accountHolder") accountHolder: String,
+    ): String
+
+    @PUT("api/$API_VERSION/account-holder/{accountHolder}/profile-image")
+    suspend fun uploadAccountPicture(
+        @Header("Authorization") token: String = "Bearer $API_KEY",
+        @Header("Content-type") value: String = "image/jpeg",
+        @Path("accountHolder") accountHolder: String,
+        @Body requestBody: RequestBody
+    ): Array<String>?
 }
