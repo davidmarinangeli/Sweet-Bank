3294b5effee27ced576c3d005a6bfe13cfd4f528
davidmarinangeli <david.marinangeli@gmail.com>
2021-01-11 16:15:34 +0100

    - new motion layout effect ( easein )

--- dashboard/src/main/java/com/davidm/ui/DashboardFragment.kt
+++ dashboard/src/main/java/com/davidm/ui/DashboardFragment.kt
@@ -1,17 +1,13 @@
 package com.davidm.ui
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.viewpager2.widget.ViewPager2
 import com.davidm.ui.databinding.FragmentDashboardBinding
 import com.davidm.utils.DateIntervalHelper
